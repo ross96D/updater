58b4d94fa1b6405c8df56757cfef14aed799661b
ross96D <abelaross96@gmail.com>
2024-03-27 19:24:15 -0400

    fix some type errors fdue to configuration changes

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -136,7 +136,14 @@ func __github_auth__(token []byte, body []byte) error {
 	if err != nil {
 		return err
 	}
-	return checkGithubSignature(string(hmac), signature, body, []byte(share.Config().GitubSignature256))
+	for _, app := range share.Config().Apps {
+		err = checkGithubSignature(string(hmac), signature, body, []byte(app.GitubSignature256))
+		if err == nil {
+			return nil
+		}
+	}
+	// TODO should this be like this.. returning the last error even if there has been other errors before?
+	return err
 }
 
 func parseUserToken(rawToken []byte) (token []byte, err error) {
@@ -176,7 +183,7 @@ func newUserToken(sub string, key []byte, expiry time.Duration) ([]byte, error) 
 }
 
 func NewUserToken(user string) ([]byte, error) {
-	return newUserToken(user, []byte(share.Config().SecretKey), share.Config().JwtUserExpiry.GoDuration())
+	return newUserToken(user, []byte(share.Config().UserSecretKey), share.Config().UserJwtExpiry.GoDuration())
 }
 
 func __user_auth__(rawToken []byte) error {
@@ -187,7 +194,7 @@ func __user_auth__(rawToken []byte) error {
 	if err != nil {
 		return err
 	}
-	return checkUserToken(token, []byte(share.Config().SecretKey))
+	return checkUserToken(token, []byte(share.Config().UserSecretKey))
 }
 
 func authFailed(w http.ResponseWriter, err error) {
