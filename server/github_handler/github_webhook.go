12c28f068dd6fe56702635c4a0e70693eac5cd40
ross96D <abelaross96@gmail.com>
2024-03-28 16:59:28 -0400

    minor refactoring

---
+++ server/github_handler/github_webhook.go
@@ -0,0 +1,51 @@
+package github_handler
+
+import (
+	"errors"
+	"fmt"
+	"slices"
+
+	"github.com/google/go-github/v60/github"
+	"github.com/ross96D/updater/share"
+	"github.com/ross96D/updater/share/configuration"
+)
+
+func HandleGithubWebhook(payload []byte, eventType string) error {
+	event, err := github.ParseWebHook(eventType, payload)
+	if err != nil {
+		return err
+	}
+	switch event := event.(type) {
+	case *github.ReleaseEvent:
+		return handleReleaseEvent(event)
+	default:
+		panic(errors.New("unhandled event"))
+	}
+}
+
+func handleReleaseEvent(event *github.ReleaseEvent) error {
+	switch *event.Action {
+	case "published", "edited":
+		return onPublishEdit(event)
+	default:
+		panic("unhandled action for release event")
+	}
+}
+
+func onPublishEdit(event *github.ReleaseEvent) error {
+	index := slices.IndexFunc(share.Config().Apps, func(e *configuration.Application) bool {
+		return *event.Repo.URL == fmt.Sprintf("github.com/%s/%s", e.Owner, e.Repo)
+	})
+	if index == -1 {
+		return errors.New("release event from repo not configured")
+	}
+	app := share.Config().Apps[index]
+	release := event.Release
+	for _, asset := range release.Assets {
+		if app.AssetName == *asset.Name {
+			return share.HandleAssetMatch(app, asset, release)
+		}
+	}
+
+	return nil
+}
---
+++ share/asset_match.go
@@ -0,0 +1,107 @@
+package share
+
+import (
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/google/go-github/v60/github"
+	"github.com/ross96D/updater/share/configuration"
+	taskservice "github.com/ross96D/updater/task_service"
+)
+
+func HandleAssetMatch(app *configuration.Application, asset *github.ReleaseAsset, release *github.RepositoryRelease) error {
+	client := NewGithubClient(app, nil)
+	rc, lenght, err := downloadableAsset(client, *asset.URL)
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	path := filepath.Join(*Config().BasePath, *asset.Name)
+	if err = CreateFile(rc, lenght, path); err != nil {
+		return err
+	}
+
+	checksum, err := GetChecksum(app, release)
+	if err != nil {
+		return err
+	}
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	verify, err := VerifyWithChecksum(checksum, f, sha256.New()) // TODO make the hash algorithm be configurable
+	if err != nil {
+		return err
+	}
+	if !verify {
+		return errors.New("asset could not be verified")
+	}
+
+	if err = taskservice.Stop(app.TaskSchedPath); err != nil {
+		return err
+	}
+
+	if err = os.Rename(app.AppPath, app.AppPath+".old"); err != nil {
+		return err
+	}
+
+	if err = os.Rename(path, app.AppPath); err != nil {
+		return err
+	}
+
+	if err = taskservice.Start(app.TaskSchedPath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func downloadableAsset(ghclient *github.Client, url string) (rc io.ReadCloser, lenght int64, err error) {
+	client := ghclient.Client()
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	req.Header.Set("Accept", "application/octet-stream")
+
+	if err != nil {
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	if resp.StatusCode >= 400 {
+		err = errors.New("invalid status code")
+		return
+	}
+	if resp.ContentLength < 0 {
+		if resp.ContentLength, err = getHeaders(ghclient, url); err != nil {
+			err = fmt.Errorf("head request: %w", err)
+			return
+		}
+	}
+	return resp.Body, resp.ContentLength, nil
+}
+
+func getHeaders(ghclient *github.Client, url string) (lenght int64, err error) {
+	client := ghclient.Client()
+	req, err := http.NewRequest(http.MethodHead, url, nil)
+	req.Header.Set("Accept", "application/octet-stream")
+	if err != nil {
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	if resp.StatusCode >= 400 {
+		err = errors.New("invalid status code")
+		return
+	}
+	lenght = resp.ContentLength
+	return
+}
