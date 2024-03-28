f424bcebeb4f16ca18aadc4e749e2041e06a463f
ross96D <abelaross96@gmail.com>
2024-03-28 15:24:52 -0400

    implement test for aggregate and direct checksum

--- share/share_test.go
+++ share/share_test.go
@@ -1,8 +1,10 @@
 package share
 
 import (
+	"context"
 	"testing"
 
+	"github.com/google/go-github/v60/github"
 	"github.com/ross96D/updater/share/configuration"
 	"github.com/stretchr/testify/assert"
 )
@@ -18,17 +20,57 @@ func TestCustomChecksum(t *testing.T) {
 }
 
 func TestAggregateChecksum(t *testing.T) {
+	client := github.NewClient(nil)
+	release, _, err := client.Repositories.GetRelease(context.Background(), "ross96D", "updater", 148914964)
+	assert.Equal(t, nil, err)
+
+	// test getting the key from the asset name
+	checksum, err := aggregateChecksum(
+		configuration.AggregateChecksum{
+			AssetName: "aggregate_checksum.txt",
+		},
+		&configuration.Application{
+			Owner:     "ross96D",
+			Repo:      "updater",
+			AssetName: "valid_key",
+		},
+		release,
+	)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "aggregate_checksum", string(checksum))
+
+	// test with a direct key name
+	key := "valid_key"
+	checksum, err = aggregateChecksum(
+		configuration.AggregateChecksum{
+			AssetName: "aggregate_checksum.txt",
+			Key:       &key,
+		},
+		&configuration.Application{
+			Owner: "ross96D",
+			Repo:  "updater",
+		},
+		release,
+	)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "aggregate_checksum", string(checksum))
 
 }
 
-// func TestDirectChecksum(t *testing.T) {
-// 	directChecksum(
-// 		configuration.DirectChecksum{AssetName: "ss"},
-// 		&configuration.Application{
-// 			Owner: "ross96D",
-// 			Repo: "updater",
-// 			Host: "github",
-// 		},
-// 		&github.RepositoryRelease{}
-// 	)
-// }
+func TestDirectChecksum(t *testing.T) {
+	client := github.NewClient(nil)
+	release, _, err := client.Repositories.GetRelease(context.Background(), "ross96D", "updater", 148914964)
+	assert.Equal(t, nil, err)
+
+	checksum, err := directChecksum(
+		configuration.DirectChecksum{AssetName: "direct_checksum.txt"},
+		&configuration.Application{
+			Owner: "ross96D",
+			Repo:  "updater",
+		},
+		release,
+	)
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "direct_checksum", string(checksum))
+}
