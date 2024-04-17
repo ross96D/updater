7632f38991c48ae8847832296ca33f61c80c83fe
ross96D <abelaross96@gmail.com>
2024-04-17 18:20:02 -0400

    add test

--- share/configuration/configuration_test.go
+++ share/configuration/configuration_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestJson(t *testing.T) {
+var key = "key"
+
+func TestApplicationJson(t *testing.T) {
 	apps := []Application{
 		{
 			Owner:               "ross",
@@ -26,6 +28,17 @@ func TestJson(t *testing.T) {
 					SystemPath: "add_asset_path",
 					Checksum:   Checksum{C: DirectChecksum{AssetName: "add_asset_checksum"}},
 				},
+				{
+					Name:       "add_asset",
+					SystemPath: "add_asset_path",
+					Checksum:   Checksum{C: AggregateChecksum{AssetName: "add_asset_checksum", Key: &key}},
+				},
+
+				{
+					Name:       "add_asset",
+					SystemPath: "add_asset_path",
+					Checksum:   Checksum{C: CustomChecksum{Command: "command", Args: []string{"arg1", "arg2"}}},
+				},
 			},
 			UseCache: true,
 		},
@@ -38,5 +51,57 @@ func TestJson(t *testing.T) {
 		err = json.Unmarshal(b, &actual)
 		require.Equal(t, nil, err)
 		assert.Equal(t, app, actual)
+	}
+}
+
+func TestConfigurationJson(t *testing.T) {
+	configs := []Configuration{
+		{
+			Port:          8932,
+			Output:        "output",
+			UserJwtExpiry: Duration(500),
+			UserSecretKey: "key",
+			Users: []User{
+				{
+					Name:     "ross",
+					Password: "123",
+				},
+				{
+					Name:     "ross2",
+					Password: "1233",
+				},
+			},
+			BasePath: "base",
+			Apps: []Application{
+				{
+					Owner:               "ross",
+					Repo:                "repo",
+					Host:                "github.com",
+					GithubWebhookSecret: "secret",
+					GithubAuthToken:     "token",
+					AssetName:           "asset",
+					TaskSchedPath:       "task_path",
+					SystemPath:          "sys_path",
+					Checksum:            Checksum{C: DirectChecksum{AssetName: "asset"}},
+					AdditionalAssets: []AdditionalAsset{
+						{
+							Name:       "add_asset",
+							SystemPath: "add_asset_path",
+							Checksum:   Checksum{C: DirectChecksum{AssetName: "add_asset_checksum"}},
+						},
+					},
+					UseCache: true,
+				},
+			},
+		},
+	}
+	for _, conf := range configs {
+		b, err := json.Marshal(conf)
+		require.Equal(t, nil, err)
+
+		var actual Configuration
+		err = json.Unmarshal(b, &actual)
+		require.Equal(t, nil, err)
+		assert.Equal(t, conf, actual)
 	}
 }
