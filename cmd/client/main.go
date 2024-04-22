bc92c33242998421b4de8704ebe0ea321fd6a9b0
ross96D <abelaross96@gmail.com>
2024-04-22 13:46:24 -0400

    add update command to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -29,8 +29,19 @@ var reloadCommand = &cobra.Command{
 	},
 }
 
+var updateCommand = &cobra.Command{
+	Use: "reload",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := update(); err != nil {
+			println(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+
 func init() {
 	rootCommand.AddCommand(reloadCommand)
+	rootCommand.AddCommand(updateCommand)
 	rootCommand.PersistentFlags().StringVarP(&baseUrl, "host", "H", "http://localhost:8081/cicd", "set the url of the updater service. The default value is http://localhost:10000")
 	rootCommand.PersistentFlags().StringVarP(&user, "user", "u", "", "user name")
 	// TODO maybe this is needed to be read from an env variable or a config file, but for now as this is for testing mainly.. just who cares
@@ -43,6 +54,33 @@ func main() {
 	if err := rootCommand.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func update() (err error) {
+	if token == "" {
+		token, err = login()
+		if err != nil {
+			return err
+		}
+	}
+	url, err := path_url.JoinPath(baseUrl, "update")
+	if err != nil {
+		return err
+	}
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Authorization", "Bearer "+token)
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return err
+	}
+	b, err := io.ReadAll(resp.Body)
+	if resp.StatusCode > 400 {
+		err = fmt.Errorf("%w %s", err, string(b))
+	}
+	return err
 }
 
 func reload() (err error) {
@@ -58,6 +96,9 @@ func reload() (err error) {
 	}
 
 	request, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		return err
+	}
 	request.Header.Add("Authorization", "Bearer "+token)
 	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
