f5dea61a2a2bdffc2a6175ea4296b5e286993f28
ross96D <abelaross96@gmail.com>
2024-04-22 14:08:00 -0400

    add update from local client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	path_url "net/url"
 	"os"
 
+	"github.com/ross96D/updater/share/configuration"
 	"github.com/spf13/cobra"
 )
 
@@ -57,6 +60,23 @@ func main() {
 }
 
 func update() (err error) {
+	apps, err := list()
+	if err != nil {
+		return err
+	}
+	if len(apps) == 0 {
+		err = fmt.Errorf("no apps listed")
+		return
+	}
+	for i, app := range apps {
+		fmt.Printf("%d - %s\n", i, app.Host+"/"+app.Owner+"/"+app.Repo)
+	}
+	var num int
+	_, err = fmt.Scan(&num)
+	if err != nil {
+		return fmt.Errorf("parsing num %w", err)
+	}
+
 	if token == "" {
 		token, err = login()
 		if err != nil {
@@ -67,20 +87,65 @@ func update() (err error) {
 	if err != nil {
 		return err
 	}
-	request, err := http.NewRequest(http.MethodGet, url, nil)
+
+	bodyBytes, err := json.Marshal(apps[num])
 	if err != nil {
-		return err
+		return fmt.Errorf("marshaling app %w", err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(bodyBytes))
+	if err != nil {
+		return fmt.Errorf("new request %w", err)
 	}
 	request.Header.Add("Authorization", "Bearer "+token)
 	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
-		return err
+		return fmt.Errorf("doing request %w", err)
 	}
 	b, err := io.ReadAll(resp.Body)
 	if resp.StatusCode > 400 {
-		err = fmt.Errorf("%w %s", err, string(b))
+		if b == nil {
+			b = []byte("")
+		}
+		err = fmt.Errorf("status code %d\n %s", resp.StatusCode, string(b))
 	}
 	return err
+}
+
+func list() (apps []configuration.Application, err error) {
+	if token == "" {
+		token, err = login()
+		if err != nil {
+			return
+		}
+	}
+
+	url, err := path_url.JoinPath(baseUrl, "list")
+	if err != nil {
+		return
+	}
+
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return
+	}
+	request.Header.Add("Authorization", "Bearer "+token)
+	resp, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		return
+	}
+	b, err := io.ReadAll(resp.Body)
+	if resp.StatusCode > 400 {
+		if b == nil {
+			b = []byte("")
+		}
+		err = fmt.Errorf("status code %d\n %s", resp.StatusCode, string(b))
+		return
+	}
+	apps = make([]configuration.Application, 0)
+	err = json.Unmarshal(b, &apps)
+	return
 }
 
 func reload() (err error) {
