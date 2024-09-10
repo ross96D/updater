72371f295805bf9dff1b3d72055e5bebc534bf69
ross96D <abelaross96@gmail.com>
2024-09-09 23:24:09 -0400

    add a server update test using dry-run

--- server/server_test.go
+++ server/server_test.go
@@ -10,11 +10,99 @@ import (
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"testing"
 
 	"github.com/ross96D/updater/server"
+	"github.com/ross96D/updater/share"
+	"github.com/ross96D/updater/share/utils"
+	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestServerEnpoints(t *testing.T) {
+	configuration := `
+	port:            7432
+	user_secret_key: "secret_key"
+	user_jwt_expiry: "2h"
+	
+	apps: [
+		{
+			auth_token: "identifier-secret-token"
+	
+			assets: [
+				{
+					name:        "zipped"
+					system_path: "/not/a/path"
+					unzip:       true
+				},
+				{
+					name: "service.zipped"
+					system_path: "/not/a/path"
+					service: "service name"
+					unzip:       true
+				},
+				{
+					name: "service.zipped.command"
+					system_path: "/not/a/path"
+					service: "service with command name"
+					unzip:       true
+					cmd: {
+						command: "npm"
+						args: ["run", "dev"]
+						path: "not/a/path"
+					}
+				},
+			]
+		},
+	]
+	
+	`
+	log.Logger = log.Logger.Output(io.Discard)
+
+	t.Run("/update with out match", func(t *testing.T) {
+		err := share.ReloadString(configuration)
+		require.NoError(t, err)
+
+		buff := &bytes.Buffer{}
+		multipartWriter := multipart.NewWriter(buff)
+		{ // Create Form field
+			fieldW, err := multipartWriter.CreateFormFile("ctm_web", "ctm_web.tar.gz")
+			require.NoError(t, err)
+			var buff *bytes.Buffer = &bytes.Buffer{}
+			buff.Write([]byte("testing"))
+			_, err = io.Copy(fieldW, buff)
+			require.NoError(t, err)
+		}
+		multipartWriter.Close()
+		req := httptest.NewRequest(http.MethodPost, "/update", buff)
+		req.Header.Add("Authorization", "identifier-secret-token")
+		req.Header.Add("dry-run", "true")
+		req.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+
+		w := httptest.NewRecorder()
+
+		server.New("", "").TestServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		data, err := io.ReadAll(res.Body)
+		require.NoError(t, err)
+
+		assert.Equal(t, 200, res.StatusCode)
+
+		println("response")
+		println(string(data))
+		lines := bytes.Split(data, []byte("\n"))
+		last := lines[len(lines)-2]
+		r := regexp.MustCompile(`\d{1,2}:\d{1,2}[A,P]M (?<Level>[^\s]+)`)
+		require.True(t, r.Match(utils.StripAnsiBytes(last)))
+		level := r.FindSubmatch(utils.StripAnsiBytes(last))[r.SubexpIndex("Level")]
+		assert.NotEqual(t, "ERR", string(level))
+	})
+}
 
 func GetData(sizeInBytes uint64) io.Reader {
 	buff := bytes.NewBuffer([]byte{})
