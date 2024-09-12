bda302a803e02c3f2a6b91065a0046e23fdf2989
ross96D <abelaross96@gmail.com>
2024-09-12 16:42:26 -0400

    add load configuration

---
+++ cmd/client/state/data.go
@@ -0,0 +1,54 @@
+package state
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+var configuration Config
+
+type Config struct {
+	State GlobalState `json:"global_state"`
+}
+
+func LoadConfig() {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		panic(err)
+	}
+	configFile := filepath.Join(configDir, "updatercli", "config")
+
+	f, err := os.Open(configFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	dec := json.NewDecoder(f)
+
+	conf := Config{}
+	err = dec.Decode(&conf)
+	if err != nil {
+		panic(err)
+	}
+	configuration = conf
+}
+
+func SaveConfig() error {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return err
+	}
+	configFile := filepath.Join(configDir, "updatercli", "config")
+
+	f, err := os.Create(configFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	enc := json.NewEncoder(f)
+	err = enc.Encode(configuration)
+	return err
+}
---
+++ cmd/client/state/state_test.go
@@ -0,0 +1,104 @@
+package state_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/ross96D/updater/cmd/client/models"
+	"github.com/ross96D/updater/cmd/client/state"
+	"github.com/ross96D/updater/server/user_handler"
+	"github.com/ross96D/updater/share/configuration"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJsonMarshal(t *testing.T) {
+	configuration := state.Config{
+		State: *state.NewState([]models.Server{
+			{
+				ServerName: "server1",
+				Url:        models.UnsafeNewURL("190.168.0.1"),
+				Apps: []user_handler.App{
+					{
+						Index: 1,
+						Application: configuration.Application{
+							AuthToken: "token",
+							Assets: []configuration.Asset{
+								{
+									Name:        "asset1",
+									ServicePath: "service1",
+								},
+								{
+									Name:        "asset2",
+									ServicePath: "service2",
+								},
+							},
+						},
+					},
+					{
+						Index: 2,
+						Application: configuration.Application{
+							AuthToken: "token",
+							Assets: []configuration.Asset{
+								{
+									Name:        "asset1",
+									ServicePath: "service1",
+								},
+								{
+									Name:        "asset2",
+									ServicePath: "service2",
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				ServerName: "server2",
+				Url:        models.UnsafeNewURL("190.68.0.2"),
+				Apps: []user_handler.App{
+					{
+						Index: 1,
+						Application: configuration.Application{
+							AuthToken: "token",
+							Assets: []configuration.Asset{
+								{
+									Name:        "asset1",
+									ServicePath: "service1",
+								},
+								{
+									Name:        "asset2",
+									ServicePath: "service2",
+								},
+							},
+						},
+					},
+					{
+						Index: 2,
+						Application: configuration.Application{
+							AuthToken: "token",
+							Assets: []configuration.Asset{
+								{
+									Name:        "asset1",
+									ServicePath: "service1",
+								},
+								{
+									Name:        "asset2",
+									ServicePath: "service2",
+								},
+							},
+						},
+					},
+				},
+			},
+		}),
+	}
+
+	b, err := json.Marshal(configuration)
+	require.NoError(t, err)
+
+	var decodedConfiguration state.Config
+	err = json.Unmarshal(b, &decodedConfiguration)
+	require.NoError(t, err)
+
+	require.Equal(t, configuration, decodedConfiguration)
+}
