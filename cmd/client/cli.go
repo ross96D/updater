219d714652d8097045346bd9dc7219bfbc5fdaa7
ross96D <abelaross96@gmail.com>
2024-09-12 22:04:46 -0400

    add debug mode on cli

--- cmd/client/cli.go
+++ cmd/client/cli.go
@@ -4,14 +4,22 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/ross96D/updater/cmd/client/pretty"
 	"github.com/ross96D/updater/cmd/client/state"
 	"github.com/ross96D/updater/cmd/client/views"
 	"github.com/spf13/cobra"
 )
 
+var cliDebug bool
+
 var cliCommand = &cobra.Command{
 	Use: "cli",
 	Run: func(cmd *cobra.Command, args []string) {
+		if cliDebug {
+			// nolint
+			pretty.ActivateDebug()
+		}
+
 		state.LoadConfig()
 		state := &state.Configuration().State
 		program := views.NewApp(state)
@@ -20,4 +28,8 @@ var cliCommand = &cobra.Command{
 			os.Exit(1)
 		}
 	},
+}
+
+func init() {
+	cliCommand.Flags().BoolVar(&cliDebug, "debug", false, "create a debug.log file on the cwd")
 }
