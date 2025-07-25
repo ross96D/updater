e5cb2855b0af786ffa8d79057d8386469b25f85a
ross96D <abelaross96@gmail.com>
2025-07-25 10:48:41 -0400

    add test case

--- share/match/cronjob_test.go
+++ share/match/cronjob_test.go
@@ -8,11 +8,32 @@ import (
 )
 
 func TestCreateJobConfigurationData(t *testing.T) {
-	input := "{\"name\": \"cronjob_name\", \"command\": \"echo Some\", \"time\": \"* * * * *\"}"
-	jobs, err := match.ValidateCronJobConfiguration([]byte(input))
-	require.NoError(t, err)
-
-	data := match.CreateJobConfigurationData(jobs)
-
-	require.Equal(t, string(data), "# cronjob_name\n* * * * * root echo Some\n")
+	inputs := []struct {
+		in  string
+		out string
+	}{
+		{
+			in:  "{\"name\": \"cronjob_name\", \"command\": \"echo Some\", \"time\": \"* * * * *\"}",
+			out: "# cronjob_name\n* * * * * root echo Some\n",
+		},
+		{
+			in: `[
+					{"name": "cronjob_name", "command": "echo Some", "time": "* * * * *"},
+					{"name": "cronjob_name2", "command": "echo Some2", "time": "10 10 * * *"}
+				]`,
+			out: `# cronjob_name
+* * * * * root echo Some
+# cronjob_name2
+10 10 * * * root echo Some2
+`,
+		},
+	}
+	for _, input := range inputs {
+		jobs, err := match.ValidateCronJobConfiguration([]byte(input.in))
+		require.NoError(t, err)
+		data := match.CreateJobConfigurationData(jobs)
+		println("AAAAAAAAAAAAA", input.in)
+		println("ZZZZZZZZZZZZZ", input.out)
+		require.Equal(t, input.out, string(data))
+	}
 }
